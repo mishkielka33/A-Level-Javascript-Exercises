0785459a439f6a815f2e89adead5b674d92dc983
mishkielka33 <627ml92@canonslade.tbft.uk>
2025-09-09 08:50:17 +0000

    started selection

---
+++ Class Exercises/2 - Javascript Selection/Task 1a - Golfing Handicap/script.js
@@ -0,0 +1,23 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button element
+    const checkButton = document.getElementById('checkButton');
+    
+    // Add click event listener to the button
+    checkButton.addEventListener('click', calculateLeague);
+});
+
+// Function to calculate which league the player belongs to
+function calculateLeague() {
+    // TODO: Get the handicap value from the input field
+    let handicap = parseFloat(document.getElementById('handicap').value);
+    let league = 0
+    // TODO: Check if the handicap is less than 13
+    if (handicap < 13){
+        league = 1
+    } else {
+        league = 2
+    }
+    // TODO: Display the appropriate message in the result paragraph
+    document.getElementById('result').textContent = `You are a league ${league} player.`
+}
