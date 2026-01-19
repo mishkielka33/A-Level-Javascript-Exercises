b92f8ba1127ef1da1bb35ecd69b5eb0ee7a9a100
mishkielka33 <627ml92@canonslade.tbft.uk>
2026-01-19 12:03:55 +0000

    followinh mouse

--- P5 Play Template/script.js
+++ P5 Play Template/script.js
@@ -1,21 +1,30 @@
 let block;
 let floor;
 let ball;
+let basket = loadImage('/assets/basket.jpg')
+
+function preload() {
+}
 
 function setup() {
 	new Canvas(1000, 600);
 	 block = new Sprite(350, 300, 50, 50, DYN);
-	 floor = new Sprite(500, 350, 1000, 10, DYN);
-	 floor.rotationSpeed = 1;
+	 floor = new Sprite(500, 350, 1000, 10, STA);
+	 basket = new Sprite(300, 300, )
 	 world.gravity.y = 9.81;
-
+	block.bounciness = 1;
+	block.rotationSpeed = 3;
 } 
 
+function controls() {
+	block.moveTowards(mouse, 0.10);
+}
+
+function draw () {
+	
+}
+
 function update() {
-	if (mouse.presses()) {
-		block.x = mouse.x;
-		block.y = mouse.y;
-		let ball = (50)
+	block.moveTowards(mouse, 0.5);
 	};
-	clear()
-}; 
+ 
